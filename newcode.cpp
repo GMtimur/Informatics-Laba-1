fa3fda2d873a4741b9101e96de598829df2ec4de
GMtimur <gmtimurtwitch@gmail.com>
2024-09-15 13:32:12 +0300

    Update-1

--- newcode.cpp
+++ newcode.cpp
@@ -4,15 +4,29 @@ using namespace std;
 
 int main() {
     float x,y;
-    cout<<"Введите координату х: "<<endl;
-    cin>>x;
-    cout<<"Введите координату y: "<<endl;
-    cin>>y;
-    if(((0 <= y)&&(y <= 4))&&((0 <= x)&&(x <= 3))&&(x >= y)) {
-        cout<<"Координаты: "<<x<<","<<y<<" входят в треугольник!"<<endl;
+    bool interface;
+    cin>>interface;
+    if(interface){ 
+        cout<<"Введите координату х: "<<endl;
+        cin>>x;
+        cout<<"Введите координату y: "<<endl;
+        cin>>y;
+        if(((0 <= y)&&(y <= 4))&&((0 <= x)&&(x <= 3))&&(x >= y)) {
+            cout<<"Координаты: "<<x<<","<<y<<" входят в треугольник!"<<endl;
+        }
+        else {
+            cout<<"Координаты: "<<x<<","<<y<<" не входят в треугольник!"<<endl;
+        }
     }
-    else {
-        cout<<"Координаты: "<<x<<","<<y<<" не входят в треугольник!"<<endl;
+    else{
+        cin>>x;
+        cin>>y;
+        if(((0 <= y)&&(y <= 4))&&((0 <= x)&&(x <= 3))&&(x >= y)) {
+            cout<<"YES"<<endl;
+        }
+        else {
+            cout<<"NO"<<endl;
+        }
     }
     return 0;
 }
