8ab762fada9a7cac30634836925bb06b1ccd6d85
GMtimur <gmtimurtwitch@gmail.com>
2024-09-25 14:47:42 +0300

    Commit

--- main.cpp
+++ main.cpp
@@ -4,57 +4,46 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    setlocale(LC_CTYPE, "rus");
     bool isHuman = false;
-    float x = 1, y = 1;
+    float x = 1.0, y = 1.0, const1 = -4.0, const2 = 3.0, const3 = 4.0;
 	if (argc <= 1 || strcmp(argv[1], "false") != 0)
 	{
 		isHuman = true;
 	}
-    if(isHuman){
-        while(x != 0 || y != 0){
+    while(x != 0 || y != 0){
+        if(isHuman)
             cout << "Введи координату х: ";
-            cin >> x;  
+        cin >> x;  
+        if(isHuman)
             cout << "Введи координату y: ";
-            cin >> y;
-            if (x <= 3 && x >= 0)
-            {
-                float y1 = 0;
-                y1 =  -4.0/3 * x + 4;
+        cin >> y;
+        if (x <= 3 && x >= 0)
+        {
+            float y1 = 0;
+            y1 =  const1/const2 * x + const3;
             if ((y <= y1) && (y >= 0))
             {
-                cout << "Координаты: " << x << ", " << y << " входят в треугольник!" << endl;
+                if(isHuman)
+                    cout << "Координаты: " << x << ", " << y << " входят в треугольник!" << endl;
+                else
+                    cout << "YES" << endl;
             }
             else
             {
-                cout << "Координаты: " << x << ", " << y << " не входят в треугольник!" << endl;
-            }
-            }
-            else{
-                cout << "Координаты: " << x << ", " << y << " не входят в треугольник!" << endl;
-            }
-        } 
-    }
-    else{
-        while(x != 0 || y != 0){
-            cin >> x;  
-            cin >> y;
-            if (x <= 3 && x >= 0)
-            {
-                float y1 = 0;
-                y1 =  -4.0/3 * x + 4;
-                if ((y <= y1) && (y >= 0))
-                {
-                    cout << "YES" << endl;
-                }
+                if(isHuman)
+                    cout << "Координаты: " << x << ", " << y << " не входят в треугольник!" << endl;
                 else
-                {
                     cout << "NO" << endl;
-                }
             }
-            else{
+        }
+        else
+        {
+            if(isHuman)
+                cout << "Координаты: " << x << ", " << y << " не входят в треугольник!" << endl;
+            else
                 cout << "NO" << endl;
-            }
-        } 
+        }
     }
     return 0;
 }
